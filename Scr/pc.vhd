88c4a4937f99d97f126ec19baf04065eae7e0e15
Shahd Mahmoud <158595641+eng-shahd@users.noreply.github.com>
2025-12-03 00:18:41 +0200

    Implement program counter (pc) in VHDL

--- Scr/pc.vhd
+++ Scr/pc.vhd
@@ -1,1 +1,29 @@
 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity pc is
+    Port (
+        clk     : in  STD_LOGIC;
+        reset   : in  STD_LOGIC;
+        pc_in   : in  STD_LOGIC_VECTOR(31 downto 0);
+        pc_out  : out STD_LOGIC_VECTOR(31 downto 0)
+    );
+end pc;
+
+architecture Behavioral of pc is
+    signal pc_reg : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+begin
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            pc_reg <= (others => '0');
+        elsif rising_edge(clk) then
+            pc_reg <= pc_in;
+        end if;
+    end process;
+
+    pc_out <= pc_reg;
+
+end Behavioral;
