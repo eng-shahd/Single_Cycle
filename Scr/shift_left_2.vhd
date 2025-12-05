c429140dea1a6566c91697da85acb13b2c232abd
ftmelbialy <168136874+ftmelbialy@users.noreply.github.com>
2025-12-06 00:50:27 +0200

    Update ShiftLeft2.vhd

--- Scr/shift_left_2.vhd
+++ Scr/shift_left_2.vhd
@@ -1,1 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity ShiftLeft2 is
+  port( 
+        input  : in std_logic_vector(31 downto 0);
+        output : out std_logic_vector(31 downto 0) );
+end entity;
+
+architecture Behavior of ShiftLeft2 is
+begin
+
+  output <= std_logic_vector(unsigned(input) sll 2);
+
+end architecture ;
