ff4f05045aae349634c2d696a984501af8ffc846
ftmelbialy <168136874+ftmelbialy@users.noreply.github.com>
2025-12-06 00:47:44 +0200

    Update sign_extend.vhd

--- Scr/sign_extend.vhd
+++ Scr/sign_extend.vhd
@@ -2,13 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity SignExtend is
+entity sign_extend is
   port( 
         input  : in std_logic_vector(15 downto 0);
 
         output : out std_logic_vector(31 downto 0) );
 end entity;			 
-architecture Behavioral of SignExtend is
+architecture Behave of sign_extend is
 begin
 
  output <= "0000000000000000" & input when (input(15) = '0') else
